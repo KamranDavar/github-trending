b59075a4be1190c7b5b72c767bccd9160ae5d31c
kamran <kamran.davar.kd@gmail.com>
2021-12-17 14:18:36 +0330

    feat(): add index file for static data

---
+++ src/staticData/index.ts
@@ -0,0 +1,3 @@
+export { programmingLanguages } from './programmingLanguages'
+export { spokenLanguages } from './spokenLanuages'
+export { dateRange } from './dateRange'
--- src/staticData/spokenLanuages.ts
+++ src/staticData/spokenLanuages.ts
@@ -1,4 +1,4 @@
-export const languages = [
+export const spokenLanguages = [
   { label: 'Persian', value: 'fa' },
   { label: 'English', value: 'en' },
   { label: 'Arabic', value: 'ar' },
